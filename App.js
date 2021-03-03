051c7412fd5b5c334496e95fca95f416d78af09a
Sirrom84 <morrisrjc@gmail.com>
2021-03-03 11:26:17 -0700

    Added carousel for planning

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import {SafeAreaView, StyleSheet} from "react-native";
+import {} from "./components/CarouselCards";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<CarouselCards />
+		</SafeAreaView>
+	);
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 50,
+	},
 });
