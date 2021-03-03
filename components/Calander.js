5406735e4a1c22347017c0966a64d44ee4210980
Sirrom84 <morrisrjc@gmail.com>
2021-03-03 13:49:28 -0700

    trying to explore enivorment

---
+++ components/Calander.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+
+export const Calander = () => {
+	return (
+		<View>
+			<Text>This Is the Calander page</Text>
+		</View>
+	);
+};
--- components/CarouselCardItem.js
+++ components/CarouselCardItem.js
@@ -1,16 +1,31 @@
-import React from "react";
-import {View, Text, StyleSheet, Dimensions, Image} from "react-native";
+import React, {useState} from "react";
+import {
+	View,
+	Text,
+	StyleSheet,
+	Dimensions,
+	Image,
+	TouchableOpacity,
+	Modal,
+} from "react-native";
 
 export const SLIDER_WIDTH = Dimensions.get("window").width + 80;
 export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
 
+const onPress = () => {
+	console.log("The page will open");
+};
+
 const CarouselCardItem = ({item, index}) => {
+	console.log(item);
 	return (
-		<View style={styles.container} key={index}>
-			<Image source={{uri: item.imgUrl}} style={styles.image} />
-			<Text style={styles.header}>{item.title}</Text>
-			<Text style={styles.body}>{item.body}</Text>
-		</View>
+		<TouchableOpacity onPress={onPress}>
+			<View style={styles.container} key={index}>
+				<Image source={{uri: item.imgUrl}} style={styles.image} />
+				<Text style={styles.header}>{item.title}</Text>
+				<Text style={styles.body}>{item.body}</Text>
+			</View>
+		</TouchableOpacity>
 	);
 };
 const styles = StyleSheet.create({
